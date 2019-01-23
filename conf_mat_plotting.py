c4f56709ed1fc535aa4c383fa7f1d8455d61af14
Shigemi ISHIDA <ishida+devel@f.ait.kyushu-u.ac.jp>
2019-01-23 19:38:54 +0900

    Working on estimation class.

---
+++ conf_mat_plotting.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+# 
+# Copyright (c) 2018, Masahiko MIYAZAKI, Shigemi ISHIDA, Kyushu University
+# All rights reserved.
+# 
+# DO NOT REDISTRIBUTE THIS PROGRAM NOR A PART OF THIS PROGRAM.
+# 
+# requires python >= ver.3.5
+# 
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#==========================================================================
+class ConfMatPlotting():
+    #----------------------------------------------------------------------
+    def __init__(self):
+        return
+
+    #----------------------------------------------------------------------
+    def finalize(self):
+        # 各回のconfusion matrixを統合（合計）
+        self.final_conf_matrix = np.sum(self.conf_matrix, axis=0)
+        # test accuracyはconfusion matrixから算出
+        self.final_accuracy = np.sum(np.diag(self.final_conf_matrix)) / np.sum(self.final_conf_matrix)
+
+        return (self.final_conf_matrix, self.final_accuracy)
+
+    #----------------------------------------------------------------------
+    def plot_confusion_matrix(self, plot_file=None, fontsize=14):
+        fig = plt.figure()
+
+        if plot_file is not None:
+            # ファイル出力のときはプロットの調整
+            # plt.rcParams['font.family'] = 'Times'           # 全体のフォント
+            plt.rcParams['font.size'] = fontsize            # フォントサイズ
+            # plt.rcParams['axes.linewidth'] = 1              # 軸の太さ
+            fig.subplots_adjust(bottom = 0.15)              # 下の余白を増やす
+
+            # EPS出力のためのおまじない
+            plt.rcParams['ps.useafm'] = True
+            plt.rcParams['pdf.use14corefonts'] = True
+            plt.rcParams['text.usetex'] = True
+
+        sns.heatmap(self.final_conf_matrix,
+                    cmap="Blues",
+                    annot=True,
+                    fmt="d",
+                    cbar=False,
+                    )
+        plt.xlabel('Estimated Area')
+        plt.ylabel('Actual Area')
+
+        if plot_file is None:
+            plt.show()
+        else:
+            plt.savefig(plot_file)
+
+        plt.close()
+        return
+
+    #----------------------------------------------------------------------
+    def load_result(self, result_file):
+        conf_mat = np.loadtxt(result_file, delimiter=',', dtype=np.int)
+        self.classes = int(np.sqrt(conf_mat.shape[1]))
+        self.conf_matrix = conf_mat.reshape(-1, self.classes, self.classes)
+        return
+
+    #----------------------------------------------------------------------
+    def load_score(self, score_file):
+        self.score = np.loadtxt(score_file, delimiter=',', dtype=np.float)
+        return
+
+#==========================================================================
+# 引数処理
+def arg_parser():
+    import argparse
+    ap = argparse.ArgumentParser()
+    ap.add_argument("result_file", type=str, action="store",
+                    help="result file to load",
+                    )
+    # ap.add_argument("-d", "--debug", action="store_true",
+    #                 help="debug mode",
+    #                 )
+    ap.add_argument("-p", "--plot", action="store",
+                    default=None,
+                    help="confusion matrix plotting output file name",
+                    )
+    ap.add_argument("-f", "--fontsize", type=int, action="store",
+                    default=None,
+                    help="plot font size",
+                    )
+    ap.add_argument("-s", "--scorefile", type=int, action="store",
+                    default=None,
+                    help="load score file",
+                    )
+    return ap
+
+#==========================================================================
+if __name__ == '__main__':
+    parser = arg_parser()
+    args = parser.parse_args()
+
+    c = ConfMatPlotting()
+
+    # 結果を読み込み
+    c.load_result(args.result_file)
+
+    # 最終結果を計算
+    c.finalize()
+
+    # confusion matrixをプロットして保存
+    if args.plot is not None:
+        if args.fontsize is not None:
+            c.plot_confusion_matrix(args.plot, fontsize=args.fontsize)
+        else:
+            c.plot_confusion_matrix(args.plot)
+    else:
+            c.plot_confusion_matrix()
+
+    print("accuracy=%.4f" % c.final_accuracy)
---
+++ estimate_svm.py
@@ -0,0 +1,206 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2018, Shigemi ISHIDA
+# All rights reserved.
+#
+# DO NOT REDISTRIBUTE THIS PROGRAM NOR A PART OF THIS PROGRAM.
+#
+
+import os
+import sys
+import numpy as np
+import importlib
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import RepeatedStratifiedKFold, train_test_split
+from imblearn.over_sampling import SMOTE
+
+import vehicles
+import conf_mat_plotting
+
+#==========================================================================
+class Estimate(conf_mat_plotting.ConfMatPlotting):
+    def __init__(self,
+                 ext_feature_class="ext_feature_shift_fft", # feature extraction class file
+                 result_file=None, # output filename for results
+                 score_file=None,  # output filename for test score
+                 ):
+        super(Estimate, self).__init__()
+        self.result_file = result_file
+        self.score_file = score_file
+
+        self.model = None          # machine learning model
+        self.results = None        # results
+        self.ext_feature = None    # feature extraction class
+        self.vehicles = None       # vehicle data class
+        self.classes = None        # number of classes (labels)
+        # feature matrix: features, label (= vehicle type id)
+        self.feature_matrix = None
+
+        # import feature extraction class
+        self.load_ext_feature_class(ext_feature_class)
+
+        return
+
+    #----------------------------------------------------------------------
+    def load_ext_feature_class(self, class_file):
+        # add feature extraction class path to the import path
+        base_path = os.path.dirname(class_file)
+        base_file, base_ext = os.path.splitext(os.path.basename(class_file))
+        sys.path.append(base_path)
+        base_name = base_file.split("_")[:2]
+
+        # import feature extraction class
+        self.ext = importlib.import_module(base_file)
+
+        return True
+
+    #----------------------------------------------------------------------
+    def load_data(self, vehicle_file, wavfile):
+        # load sound data
+        print("load sound data %s" % wavfile)
+        self.ext_feature = self.ext.ExtFeature(wavfile)
+        self.ext_feature.load_sound()
+        # load vehicle data
+        print("load vehicle data %s" % vehicle_file)
+        self.vehicles = vehicles.Vehicles(vehicle_file, self.ext_feature)
+        self.vehicles.load_data()
+
+        # calculate features
+        print("calculate features")
+        self.feature_matrix = self.vehicles.calc_features()
+
+        # number of classes (labels)
+        self.classes = len(self.vehicles.data.type.unique())
+
+        return
+
+    #----------------------------------------------------------------------
+    def define_model(self):
+        # release model
+        del self.model
+        # reload model
+        self.model = SVC()
+
+        return self.model
+
+    #----------------------------------------------------------------------
+    def eval(self, x_train, y_train, x_test, y_test):
+        # recompile model
+        self.define_model()
+        # training
+        self.model.fit(x_train, y_train)
+        test_score = self.model.score(x_train, y_train)
+
+        # estimation
+        y_est = self.model.predict(x_test)
+
+        # generate confusion matrix
+        conf_mat = confusion_matrix(y_test, y_est)
+
+        return test_score, conf_mat
+
+    #----------------------------------------------------------------------
+    def cross_validation(self, folds=10, repeat=1):
+        # data
+        x = self.feature_matrix[:,:-1]
+        # label
+        y = self.feature_matrix[:,-1]
+
+        # resample data to balance the training/test data
+        smote = SMOTE()
+        x_resamp, y_resamp = smote.fit_sample(x, y)
+
+        # folded validation
+        skf = RepeatedStratifiedKFold(n_splits=folds, n_repeats=repeat)
+
+        count = 0
+        for train_idx, test_idx in skf.split(x_resamp, y_resamp):
+            print("iter=%d" % count)
+            score, conf_mat = self.eval(x_resamp[train_idx], y_resamp[train_idx],
+                                        x_resamp[test_idx],  y_resamp[test_idx])
+            self.save_result(conf_mat)
+            self.save_score(score)
+            count += 1
+
+        return True
+
+    #----------------------------------------------------------------------
+    def save_result(self, c_mat):
+        if self.result_file is None:
+            return False
+
+        with open(self.result_file, 'a') as f:
+            np.savetxt(f,
+                       c_mat.reshape(1,-1),
+                       fmt=["%d"]*(self.classes**2),
+                       delimiter=",",
+                       )
+
+        return True
+
+    #----------------------------------------------------------------------
+    def save_score(self, score):
+        if self.score_file is None:
+            return False
+
+        with open(self.score_file, 'a') as f:
+            np.savetxt(f,
+                       np.array([score]),
+                       fmt=["%f"]*2,
+                       delimiter=",",
+                       )
+
+        return True
+
+#==========================================================================
+# handle arguments
+def arg_parser():
+    import argparse
+    ap = argparse.ArgumentParser()
+    ap.add_argument("vehicle_info", type=str, action="store",
+                    help="vehicle_info",
+                    )
+    ap.add_argument("wavfile", type=str, action="store",
+                    help="vehicle sound wavfile",
+                    )
+    ap.add_argument("-f", "--folds", type=int, action="store",
+                    default=10,
+                    help="number of folding in cross-validation",
+                    )
+    ap.add_argument("-o", "--outfile", action="store",
+                    default="result.csv",
+                    help="result output file name",
+                    )
+    ap.add_argument("-s", "--scorefile", action="store",
+                    default="score.csv",
+                    help="score output file name",
+                    )
+    ap.add_argument("-p", "--plot", action="store",
+                    default=None,
+                    help="confusion matrix plotting output file name",
+                    )
+    ap.add_argument("-r", "--repeats", type=int, action="store",
+                    default=1,
+                    help="number of repeats of cross-validation",
+                    )
+    return ap
+
+#==========================================================================
+if __name__ == '__main__':
+    parser = arg_parser()
+    args = parser.parse_args()
+
+    e = Estimate()
+
+    if args.outfile is not None:
+        e.result_file = args.outfile
+    if args.scorefile is not None:
+        e.score_file = args.scorefile
+
+    e.load_data(args.vehicle_info, args.wavfile)
+    e.cross_validation(folds=args.folds, repeat=args.repeats)
+
+    # plot confusion matrix
+    if args.plot is not None:
+        c.plot_confusion_matrix(args.plot)
