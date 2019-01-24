f4bf474d91141c701cc9be7757567c0b7ba81609
Shigemi ISHIDA <ishida+devel@f.ait.kyushu-u.ac.jp>
2019-01-24 19:26:44 +0900

    Support automated eval.

--- main.py
+++ main.py
@@ -6,67 +6,90 @@
 # DO NOT REDISTRIBUTE THIS PROGRAM NOR A PART OF THIS PROGRAM.
 #
 
+import sys
+import os
 import numpy as np
+import shutil
+import importlib
+import datetime
 
-from sound_shift_fft import SoundShiftFFT
+OUTDIR="results"
 
-#==========================================================================
+#======================================================================
 # 引数処理
 def arg_parser():
     import argparse
     ap = argparse.ArgumentParser()
-    ap.add_argument("infile", type=str, action="store",
-                    help="infile",
-                    )
-    ap.add_argument("outfile", type=str, action="store",
-                    nargs="?",
-                    default="output.dat",
-                    help="output file name",
-                    )
-    ap.add_argument("-t", action="store",
-                    dest="test",
-                    help="test parameter",
-                    )
-    ap.add_argument("-d", "--debug", action="store_true",
-                    help="debug mode",
-                    )
-    ap.add_argument("-f", "--files", action="append",
-                    default=[],
-                    help="file names",
-                    )
-    ap.add_argument("-l", "--limit", action="store",
-                    type=str,
-                    default=None,
-                    help="Limit range of analysis by time as \"[start:end]\" or \"start\"",
-                    )
-    ap.add_argument("offset", type=int, action="store", nargs="*",
-                    default=[0],
-                    help="set of offset values for plotting (default: 0)",
+    ap.add_argument("-c", "--conffile", type=str, action="store",
+                    default="config.py",
+                    help="config file",
                     )
     return ap
 
-#==========================================================================
+#----------------------------------------------------------------------
+# 推定用クラスを読み込み
+def load_est_class(class_file):
+    # classファイルのをimport pathに追加
+    base_path = os.path.dirname(class_file)
+    base_file, base_ext = os.path.splitext(os.path.basename(class_file))
+    sys.path.append(base_path)
+    base_name = base_file.split("_")[:2]
+
+    return importlib.import_module(base_file)
+
+#======================================================================
 if __name__ == '__main__':
     parser = arg_parser()
     args = parser.parse_args()
 
-    print("infile = " + args.infile)
-    print("outfile = " + args.outfile)
-    if args.test:
-        print("test =" + args.test)
+    if not os.path.exists(OUTDIR):
+        os.makedirs(OUTDIR)
 
-    start = 0.
-    end = 10
-    if args.limit is not None:
-        limit = args.limit.replace("[", "").replace("]", "")
-        rng = limit.split(":")
-        if len(rng) <= 1:
-            start = float(rng[0])
-            end   = start + args.step/2
-        else:
-            start = float(rng[0])
-            end   = float(rng[1])
+    # 現在時刻を取得し、データファイルのベースとする
+    now = datetime.datetime.today().strftime("%Y%m%d_%H%M")
+    save_base = OUTDIR + "/" + now
 
-    t = SoundShiftFFT()
-    print(t.glob_method())
-    del t
+    # 設定ファイルのパスをimport pathに追加
+    base_path = os.path.dirname(args.conffile)
+    base_file, base_ext = os.path.splitext(os.path.basename(args.conffile))
+    sys.path.append(base_path)
+
+    try:
+        config = importlib.import_module(base_file)
+    except ImportError:
+        sys.stderr.write('Error: Ignore missing config file "' + args.conffile + '"\n')
+        sys.exit(1)
+
+    print(args.conffile + " saved as " + save_base + "_config.py")
+
+    # 設定ファイルをコピーしておく
+    shutil.copy(args.conffile, save_base + "_config.py")
+
+    #--------------------------------------------------
+    # 推定クラスを読み込み
+    est_class = load_est_class(config.est_class)
+
+    # 推定クラスをインスタンス化
+    e = est_class.Estimate(ext_feature_class=config.ext_feature_class,
+                           result_file="%s_result.csv" % (save_base),
+                           score_file="%s_score.csv" % (save_base),
+                           winsize=config.winsize,
+                           )
+
+    # データ読み込み
+    e.load_data(config.vehicle_info, config.wavfile)
+
+    # 推定
+    e.cross_validation(folds=config.folds, repeat=config.repeats)
+
+    # 最終結果をとりまとめ
+    e.load_result(e.result_file)
+    e.finalize()
+    print("accuracy=%.4f" % e.final_accuracy)
+
+    # confusion matrixをプロット
+    if config.plot:
+        plotfile = "%s_result.png" % (save_base)
+        if type(config.plot) is str:
+            plotfile = config.plot
+        e.plot_confusion_matrix(plotfile)
