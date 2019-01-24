e04fb9f7471ae4c2cd9339be3dea1ffeae1db2c1
Shigemi ISHIDA <ishida+devel@f.ait.kyushu-u.ac.jp>
2019-01-24 18:58:34 +0900

    Update rsync script.

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $0`
 
-RSYNC="rsync --exclude=*-eps-converted-to.pdf --exclude=.FBCIndex --exclude=.FBCLockFolder/ --exclude=.DS_Store --exclude=.git/ --exclude=.svn/ --exclude=python2.7 --exclude=python3.6m --exclude=.Python --exclude=bin/ --exclude==include/ --exclude=lib/ --exclude=__pycache__/ --exclude=patches/"' --exclude=*.pyc --exclude=*/*.pyc'" -avrz ${@:2}"
+RSYNC="rsync --exclude=*-eps-converted-to.pdf --exclude=.FBCIndex --exclude=.FBCLockFolder/ --exclude=.DS_Store --exclude=.git/ --exclude=.svn/ --exclude=python2.7 --exclude=python3.6m --exclude=.Python --exclude=bin/ --exclude=include/ --exclude=lib/ --exclude=__pycache__/ --exclude=patches/"' --exclude=*.pyc --exclude=*/*.pyc'" -avrz ${@:2}"
 SSH="ssh"
 
 case "$1" in
