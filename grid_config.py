4702938e5dcb6b37041378b419d77816b5f771b8
Shigemi ISHIDA <ishida+devel@f.ait.kyushu-u.ac.jp>
2019-01-30 07:54:54 +0900

    Working on grid config class.

--- grid_config.py
+++ grid_config.py
@@ -10,16 +10,21 @@ import os
 import sys
 import importlib
 import re
+import datetime
 
 #==========================================================================
 class GridConfig():
-    def __init__(self):
+    def __init__(self, config_out, param_out=None):
+        self.config_out = config_out # 出力configファイル名（上書き）
+        self.param_out = param_out   # パラメータの組み合わせの保存先（追記, Noneなら出力しない）
         return
 
     #----------------------------------------------------------------------
     def load_param_set(self, param_file):
         # パラメータ情報ファイルを読み込み
         self.param_file = self.load_class(param_file)
+        # 制約条件チェック関数
+        self.constraints = self.param_file.constraints
 
         # パラメータ名を取得
         self.param_names = list(self.param_file.__dict__.keys())
@@ -30,8 +35,21 @@ class GridConfig():
             ))
 
         self.param_set = {}
+        self.param_len = {}
         for param_name in self.param_names:
             self.param_set[param_name] = self.param_file.__dict__[param_name]
+            self.param_len[param_name] = len(self.param_set[param_name])
+        return
+
+    #----------------------------------------------------------------------
+    def exec_grid(self):
+        return
+
+    #----------------------------------------------------------------------
+    def generate_single_config(self, param):
+        with open(self.config_out, "w") as f:
+            pass
+
         return
 
     #----------------------------------------------------------------------
@@ -51,11 +69,15 @@ def arg_parser():
     import argparse
     ap = argparse.ArgumentParser()
     ap.add_argument("param_file", type=str, action="store",
-                    help="parameter information file",
+                    help="input file including parameter set",
                     )
-    ap.add_argument("-o", "--output", type=int, action="store",
+    ap.add_argument("-c", "--config_out", type=str, action="store",
                     default=None,
-                    help="output file storing parameter combinations",
+                    help="output config file name",
+                    )
+    ap.add_argument("-o", "--param_out", type=str, action="store",
+                    default=None,
+                    help="output file name storing parameter combination set",
                     )
     return ap
 
@@ -66,7 +88,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # grid searchクラスをインスタンス化
-    g = GridConfig()
-    
+    if args.config_out is None:
+        args.config_out = datetime.datetime.today().strftime("config_%s.py")
+    g = GridConfig(args.config_out, args.param_out)
+
     # パラメータ情報を読み込み
     g.load_param_set(args.param_file)
