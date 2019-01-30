7c60cbc5ac1569f14f1196ecaaf5e7547547a8f5
Shigemi ISHIDA <ishida+devel@f.ait.kyushu-u.ac.jp>
2019-01-30 23:36:01 +0900

    Copy parameter info.

--- grid.sh
+++ grid.sh
@@ -24,18 +24,26 @@ param_csv=$1
 now=$(date +%Y%m%d_%H%M)
 
 # 2列目がconfigファイル情報
-conf_files=$(cat $param_csv \
+conf_files=$(cat ${param_csv} \
 		 | egrep -v "^#" \
 		 | cut -d',' -f2)
-max_cnt=$(tail -1 $param_csv \
+max_cnt=$(tail -1 ${param_csv} \
 	       | cut -d',' -f1)
 
+# 1つ目のconfigに書かれている出力先にパラメータ情報csvファイルをコピー
+topconf=$(echo "$conf_files" | head -1)
+outdir=$(grep "outdir" ${topconf} \
+	     | tr -d ' ' \
+	     | cut -d'=' -f2 \
+	     | tr -d '"')
+cp ${param_csv} ${outdir}/${now}_$(basename ${param_csv})
+
 cnt=0
-echo "$conf_files" \
+echo "${conf_files}" \
     | while read f; do
     echo "--------------------------------------------------"
-    echo "grid: $cnt/$max_cnt"
+    echo "grid: ${cnt}/${max_cnt}"
     date
-    python3 main.py -c $f -b ${now}_${cnt}
-    cnt=`expr $cnt + 1`
+    python3 main.py -c ${f} -b ${now}_${cnt}
+    cnt=`expr ${cnt} + 1`
 done
